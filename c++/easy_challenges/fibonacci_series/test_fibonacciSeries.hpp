98967a3eb42f05da062d1afff649d040844ec930
Seth <sethtucker88@gmail.com>
2016-11-09 18:11:00 -0600

    Changed file extension *.h to *.hpp

--- c++/easy_challenges/fibonacci_series/test_fibonacciSeries.hpp
+++ c++/easy_challenges/fibonacci_series/test_fibonacciSeries.hpp
@@ -1,5 +1,5 @@
 /*
-Program: test_fibonacciSeries.h
+Program: test_fibonacciSeries.hpp
 Author: Seth Tucker
 Date: 11/9/2016
 Last Modified: 11/9/2016
@@ -11,7 +11,7 @@ This header file is only used for unit testing
 #define _TEST_FIBONACCI_SERIES_H_
 
 //Tells Catch to provide a main()
-#define CATCH_CONFIG_MAIN
+//#define CATCH_CONFIG_MAIN
 
 //Function definitions
 long computeFib(long number);
